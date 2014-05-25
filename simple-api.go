b6dda28508c8d270de49c59bde2944551e38ee12
Paolo Ibarra <jpibarra1130@gmail.com>
2014-05-25 22:12:42 +0800

    Added sample api.
    Signed-off-by: Paolo Ibarra <jpibarra1130@gmail.com>

---
+++ simple-api.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/sample", sample).Methods("GET")
+  r.HandleFunc("/", sample).Methods("GET")
+
+	http.Handle("/", r)
+
+	log.Println("Server started. Listening...")
+	http.ListenAndServe(":3000", nil)
+}
+
+func sample(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello there."))
+}
