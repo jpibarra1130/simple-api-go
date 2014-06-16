2eca050da201b0ba3ca646d864f58de033d939af
Paolo Ibarra <jpibarra1130@gmail.com>
2014-06-17 07:47:27 +0800

    Moved dbMap initialization in initializer.go

--- controllers/initializer.go
+++ controllers/initializer.go
@@ -1,9 +1,35 @@
 package controllers
 
 import (
+	"bitbucket.org/liamstask/goose/lib/goose"
+	"database/sql"
 	"flag"
+	"github.com/coopernurse/gorp"
+	"github.com/jpibarra1130/simple-api-go/models"
+	"log"
+	"os"
 )
 
 // global options. available to any subcommands. This was taken from goose library
 var flagPath = flag.String("path", "db", "folder containing db info")
 var flagEnv = flag.String("env", "development", "which DB environment to use")
+
+func initDb() *gorp.DbMap {
+	conf, err := goose.NewDBConf(*flagPath, *flagEnv)
+
+	if err != nil {
+		log.Fatal(err)
+		return nil
+	}
+
+	log.Printf("Connecting to %v, %v", conf.Driver.Name, conf.Driver.OpenStr)
+
+	db, err := sql.Open(conf.Driver.Name, conf.Driver.OpenStr)
+
+	dbmap := &gorp.DbMap{Db: db, Dialect: gorp.MySQLDialect{"InnoDB", "UTF8"}}
+
+	dbmap.TraceOn("[gorp]", log.New(os.Stdout, "myapp:", log.Lmicroseconds))
+	dbmap.AddTableWithName(models.User{}, "users").SetKeys(true, "Id")
+
+	return dbmap
+}
--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -1,13 +1,9 @@
 package controllers
 
 import (
-	"bitbucket.org/liamstask/goose/lib/goose"
-	"database/sql"
-	"github.com/coopernurse/gorp"
 	"github.com/jpibarra1130/simple-api-go/models"
 	_ "github.com/ziutek/mymysql/godrv"
 	"log"
-	"os"
 )
 
 func RegisterUser(email string, password string) bool {
@@ -15,25 +11,11 @@ func RegisterUser(email string, password string) bool {
 
 	user := models.NewUser(email, password)
 
-	conf, err := goose.NewDBConf(*flagPath, *flagEnv)
-
-	if err != nil {
-		log.Fatal(err)
-		return false
-	}
-
-	log.Printf("Connecting to %v, %v", conf.Driver.Name, conf.Driver.OpenStr)
-
-	db, err := sql.Open(conf.Driver.Name, conf.Driver.OpenStr)
-
-	dbmap := &gorp.DbMap{Db: db, Dialect: gorp.MySQLDialect{"InnoDB", "UTF8"}}
-	dbmap.TraceOn("[gorp]", log.New(os.Stdout, "myapp:", log.Lmicroseconds))
-
-	dbmap.AddTableWithName(models.User{}, "users").SetKeys(true, "Id")
-
+	// initialize the DbMap
+	dbmap := initDb()
 	defer dbmap.Db.Close()
 
-	err = dbmap.Insert(&user)
+	err := dbmap.Insert(&user)
 
 	if err != nil {
 		log.Printf("Error: ", err)
