a96157f0011a413992ec6145a6373fa45d7741d1
Paolo Ibarra <jpibarra1130@gmail.com>
2014-06-16 23:00:32 +0800

    Used BigInt for dates in user table

--- db/migrations/20140605234112_CreateUsers.sql
+++ db/migrations/20140605234112_CreateUsers.sql
@@ -4,8 +4,8 @@ CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `email` varchar(255) NOT NULL DEFAULT '',
   `hashed_password` varchar(255) NOT NULL DEFAULT '',
-  `created_at` datetime DEFAULT NULL,
-  `updated_at` datetime DEFAULT NULL,
+  `created_at` BIGINT DEFAULT NULL,
+  `updated_at` BIGINT DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `index_users_on_email` (`email`)
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
