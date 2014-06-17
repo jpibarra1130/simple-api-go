3eab8b94c4b52f03e8ce8863d4c1ae42433350f0
Paolo Ibarra <jpibarra1130@gmail.com>
2014-06-17 22:49:29 +0800

    Used initDb() to get the dbMap

--- controllers/post_controller.go
+++ controllers/post_controller.go
@@ -1,31 +1,16 @@
 package controllers
 
 import (
-	"bitbucket.org/liamstask/goose/lib/goose"
-	"database/sql"
-	"github.com/coopernurse/gorp"
 	"github.com/jpibarra1130/simple-api-go/models"
-	_ "github.com/ziutek/mymysql/godrv"
 	"log"
 )
 
 func GetPosts() []models.Post {
-	conf, err := goose.NewDBConf(*flagPath, *flagEnv)
-
-	if err != nil {
-		log.Fatal(err)
-		return nil
-	}
-
-	log.Printf("Connecting to %v, %v", conf.Driver.Name, conf.Driver.OpenStr)
-
-	db, err := sql.Open(conf.Driver.Name, conf.Driver.OpenStr)
-
-	dbmap := &gorp.DbMap{Db: db, Dialect: gorp.MySQLDialect{"InnoDB", "UTF8"}}
+	dbmap := initDb()
 	defer dbmap.Db.Close()
 
 	var posts []models.Post
-	_, err = dbmap.Select(&posts, "select id, title, body from posts order by id")
+	_, err := dbmap.Select(&posts, "select id, title, body from posts order by id")
 
 	if err != nil {
 		log.Printf("Error: ", err)
